1c2e74acc26683f4f99d9dede709f2afe31b49c5
Elizaveta3112 <76566371+Elizaveta3112@users.noreply.github.com>
2020-12-29 13:34:28 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+server = Flask(__name__)
+
+messages = [
+{'username':'vikvas','text':'Привет'},
+{'username':'egvas','text':'Здравствуйте!'},
+{'username':'julia_a','text':'Добрый день'}
+]
+
+
+
+@server.route('/')
+def hello():
+return '''Hello, World!
+
+
+<a target="_blank" href=/index>Index</a>'''
+@server.route('/get_messages')
+def get_messages():
+return{
+'messages': messages
+}
+
+@server.route('/index')
+def index():
+name = 'Лиза'
+return render_template("index.html")
+
+@server.route('/day-<num>')
+def day(num):
+return render_template(f"day-{num}.html")
